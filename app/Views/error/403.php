f64c86fec235ba09d5fecbaaa797f670839cda8e
sarthakpandyaa <sarthakpandya894@gmail.com>
2026-08-27 18:56:37 +0530

    error

---
+++ app/Views/error/403.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html><head><meta charset="UTF-8"><title>403</title>
+<link rel="stylesheet" href="/stockpilot/public/assets/css/app.css"></head>
+<body style="display:flex;align-items:center;justify-content:center;min-height:100vh;background:var(--canvas);">
+<div class="card" style="text-align:center;padding:40px;">
+<h1>403</h1><p>You don't have permission to access this page.</p>
+</div></body></html>
